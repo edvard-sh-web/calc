2347e58ed5b7912479dc03c90acb0d27a2940511
edvard-sh-web <edward.shushkovskiy@gmail.com>
2023-11-26 20:19:27 +0200

    Complete working version of calculator app

---
+++ code.js
@@ -0,0 +1,140 @@
+let displayValue = document.querySelector("#output");
+let lastCalc = document.querySelector("#last-calc")
+let operatorPressed = false
+let equalsPressed = false
+
+displayValue.textContent = "";
+let n1, n2, operator;
+console.log(displayValue.offsetWidth);
+let digits = document.querySelectorAll(".digit");
+digits.forEach((el) => {
+  el.addEventListener("click", () => {
+    if (displayValue.textContent === "0") {return}
+    if (displayValue.offsetWidth > 210
+      ) {alert("You've reached the input limit")
+        return}
+    popDisplay(el);
+  });
+});
+
+let dot = document.querySelector(".dot");
+dot.addEventListener("click", () => {
+  if (
+    displayValue.textContent.split(".").length >= 2 ||
+    displayValue.textContent === ""
+  ) {
+    return;
+  }
+  popDisplay(dot);
+});
+
+let zero = document.querySelector(".zero");
+zero.addEventListener("click", () => {
+  if (displayValue.textContent.length > 7 || displayValue.textContent === "0") {
+    return;
+  }
+  popDisplay(zero);
+});
+
+let operators = document.querySelectorAll(".operator");
+operators.forEach((el) => {
+  el.addEventListener("click", () => {
+    if(operatorPressed && displayValue.textContent !== ""){
+     
+      lastCalc.textContent = operate(Number.parseFloat(lastCalc.textContent), Number.parseFloat(displayValue.textContent), operator) 
+                              + " " + el.textContent
+      displayValue.textContent = ""
+      operator = el.textContent;
+
+      return
+    }
+    operator = el.textContent;
+    if(operatorPressed){
+    popLastCalc(Number.parseFloat( lastCalc.textContent), operator)
+    }else{
+      popLastCalc(displayValue.textContent, operator)
+    }
+    displayValue.textContent = ""
+    operatorPressed = true
+    equalsPressed = false
+  });
+});
+
+function popDisplay(el) {
+
+  displayValue.textContent = displayValue.textContent + el.textContent;
+}
+function popLastCalc(s, operator, s2="") {
+  lastCalc.textContent = s + " " + operator + " " + s2
+}
+function clear (){
+  displayValue.textContent = ""
+  lastCalc.textContent = ""
+  n1 = null
+  n2 = null
+  operatorPressed = false
+}
+document.querySelector(".clear").addEventListener("click", clear)
+
+function deleteDigit () {
+  if(displayValue.textContent){
+    displayValue.textContent = displayValue.textContent.substring(0, displayValue.textContent.length - 1)
+  }
+}
+document.querySelector(".delete").addEventListener("click", deleteDigit)
+
+
+document.querySelector(".equals").addEventListener("click", () => {
+  operatorPressed = false
+
+  if(equalsPressed){
+    n1 = Number.parseFloat(displayValue.textContent) 
+    
+    popLastCalc(displayValue.textContent, operator, n2)
+  }else{
+    n1 = Number.parseFloat(lastCalc.textContent) 
+  n2 = Number.parseFloat(displayValue.textContent) 
+  }
+  equalsPressed = true
+  displayValue.textContent = Math.round(operate(n1, n2, operator)* 1000000) /  1000000
+
+
+  
+})
+function add(n1, n2) {
+  return n1 + n2;
+}
+function substract(n1, n2) {
+  return n1 - n2;
+}
+
+function multiply(n1, n2) {
+  return n1 * n2;
+}
+
+function divide(n1, n2) {
+  return n1 / n2;
+}
+
+function operate(n1, n2, operator) {
+  switch (operator) {
+    case "+":
+      return add(n1, n2);
+      break;
+
+    case "-":
+      return substract(n1, n2);
+      break;
+
+    case "×":
+      return multiply(n1, n2);
+      break;
+
+    case "÷":
+      return divide(n1, n2);
+      break;
+
+    default:
+      break;
+  }
+}
